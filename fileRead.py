23f4389e2f6c11a5557ea01853a889b7d9bd3bbd
Amran Hossain <amran.h.akash@gmail.com>
2024-11-02 12:54:55 +0600

    using loop to read

--- fileRead.py
+++ fileRead.py
@@ -3,8 +3,11 @@
 file2 = open("textFile2.txt", "w")
 file2.write("Hi this a automated file.\nI generated this file using the write function.\nIt is the second day I am learning the file handling in Python.\nAnd it keeps on getting interesting!")
 
-file2 = open("textFile2.txt", "r")
-print(file2.read())
 
-# file2 = open("textFile2.txt", "a")
-# file2.write("Hi, this is a new line")
+file2 = open("textFile2.txt", "a")
+file2.write("\nHi, this is a new line")
+file2.close
+
+file2 = open("textFile2.txt", "r")
+for i in file2:
+    print(i)
