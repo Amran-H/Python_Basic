1c351d3654a854c368e858c05ea5c49c97dfed3b
Amran Hossain <amran.h.akash@gmail.com>
2024-11-04 18:21:28 +0600

    error block added

--- GguessingGame.py
+++ GguessingGame.py
@@ -3,16 +3,21 @@ import random
 Number = random.randint(1, 100)
 print("Guess the number between 1 - 100")
 
-for attempts in range(1, 1000):
-    
-    your_guess = int(input("Enter your guess: "))
+for attempts in range(1, 100):
+    try:  
+        your_guess = int(input("Enter your guess: "))
+    except ValueError:
+        print("Print a valid number")
+        continue
     
     if your_guess == Number:
-        print(f"Congratulations! You guessed the number  in {attempts} attempts")
+        print(f"Congratulations! You guessed the number in {attempts} attempts")
+        break
     elif your_guess > Number:
         print("High! try again")
     elif your_guess < Number:
         print("Low! try again")
+        
 
 
 
