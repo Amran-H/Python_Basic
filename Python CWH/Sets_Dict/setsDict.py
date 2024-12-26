11dc486f41439f83ad20d875be8aa69085e702e3
Amran Hossain <amran.h.akash@gmail.com>
2024-12-26 21:14:32 +0600

    prblems and problems

---
+++ Python CWH/Sets_Dict/setsDict.py
@@ -0,0 +1,7 @@
+e = set()
+for i in range(9):
+    num = int(input("Enter your number: "))
+    e.add(num)
+
+print(e)
+print(len(e))
--- problem_solving/leap_year.py
+++ problem_solving/leap_year.py
@@ -1,10 +1,22 @@
-year = int(input("Enter a year: "))
+# year = int(input("Enter a year: "))
 
-if (year % 400 == 0) and (year % 100 ==0):
-    print(year, "is a leap year")
+# if (year % 400 == 0) and (year % 100 ==0):
+#     print(year, "is a leap year")
     
-elif (year % 4 == 0) and (year % 100 !=0):
-    print(year, "is a leap year")
+# elif (year % 4 == 0) and (year % 100 !=0):
+#     print(year, "is a leap year")
     
+# else:
+#     print(year, "is not a leap year")
+
+
+year = int(input("Enter a year to check whether it is a leap year or not: "))
+
+if year%400==0 and year%100==0:
+    print(f"{year} is a leap year!")
+    
+elif year%4==0 and year%100!=0:
+    print(f"{year} is a leap year!")
+
 else:
-    print(year, "is not a leap year")
+    print(f"{year} is not a leap year!")
