d44fbc34984720f216f8b6f1afc806530c28554b
Amran Hossain <amran.h.akash@gmail.com>
2025-01-05 16:36:46 +0600

    spam

---
+++ Python CWH/Sets_Dict/If_Else/prb_2.py
@@ -0,0 +1,7 @@
+comment = input("Enter your comment: ")
+
+if (comment == "buy now" or comment == "subscribe this" or comment == "click this" or comment == "make a lot of money"):
+    print("Spam")
+
+else:
+    print("Not spam")
