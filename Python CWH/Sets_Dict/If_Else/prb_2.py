0ad81c76c0471cb9a0aea7846879654907514f53
Amran Hossain <amran.h.akash@gmail.com>
2025-01-05 16:40:25 +0600

    spam2

--- Python CWH/Sets_Dict/If_Else/prb_2.py
+++ Python CWH/Sets_Dict/If_Else/prb_2.py
@@ -1,6 +1,19 @@
+# comment = input("Enter your comment: ")
+
+# if (comment == "buy now" or comment == "subscribe this" or comment == "click this" or comment == "make a lot of money"):
+#     print("Spam")
+
+# else:
+#     print("Not spam")
+
+p1 = "Make a lot of money"
+p2 = "buy now"
+p3 = "subscribe this"
+p4 = "click this"
+
 comment = input("Enter your comment: ")
 
-if (comment == "buy now" or comment == "subscribe this" or comment == "click this" or comment == "make a lot of money"):
+if ((p1 in comment) or (p2 in comment) or (p3 in comment) or (p4 in comment)):
     print("Spam")
 
 else:
